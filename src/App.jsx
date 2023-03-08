ee9956f9d1f94eacd92356aa29aeb0f9a8703512
Tanvir Hossan <tanvirhossan27@gmail.com>
2023-03-08 15:37:30 +0600

    Title and form input design complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+
 function App() {
-  return <h1>Hello </h1>;
+  const [task, setTask] = useState("");
+  return (
+    <>
+      <div className="lg:px-[100px] text-center lg:mt-[100px]">
+        {/* Header  */}
+        <div>
+          <h2 className="font-bold text-[32px]">Drag & Drop</h2>
+          <h1 className="font-bold text-[40px]">Lean Kanban Board</h1>
+        </div>
+        {/* Inpur form */}
+        <form className="mt-8">
+          <input
+            type="text"
+            placeholder="Create New Task"
+            className="input input-bordered w-full max-w-sm"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          />
+          <button type="submit" className="btn ml-4">
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default App;
